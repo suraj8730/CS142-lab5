64f93dbd4e931bb881b1ea961874c00026628b1e
Suraj Kumar Patel <42140899+suraj8730@users.noreply.github.com>
2019-02-09 00:07:17 +0530

    Add files via upload

---
+++ l.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+usimg namespace std;
+void qu(int arr[],int low,int high)
+{
+if(high>low){
+    int pivot=high;
+    int p=partition(arr,l,h,pivot)
+    qu(arr,l,p-1);qu(arr,p+1,h);
+
+
+
+}
+int partition(int arr[],l,h,pivot)
+else
+{
+    return 0;
+}
+
+
+
+}
+
+int main(){
+    cin<<n;
+    int arr[n];
+    for(i=0;i<n;i++)
+        cin<<arr[i];
+    cout<<arr[n];
+    qu(arr,0,n-1);
+
+
+
+
+
+}
